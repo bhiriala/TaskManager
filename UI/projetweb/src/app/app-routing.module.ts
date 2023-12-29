890fe7a5acf4e1e83c9b0164eec8622c9dd4c8c8
Sarra <sarrasoussia2001@gmail.com>
2023-12-29 01:12:02 +0100

    first commit by sarra

--- UI/projetweb/src/app/app-routing.module.ts
+++ UI/projetweb/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ContainerComponent } from './components/container/container.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   {path:'container', component: ContainerComponent},
   {path:'header', component: HeaderComponent},
+  {path:'signup', component: SignupComponent},
+  {path:'signin', component: SigninComponent}
+
+
 ];
 
 @NgModule({
--- UI/projetweb/src/app/app.module.ts
+++ UI/projetweb/src/app/app.module.ts
@@ -18,6 +18,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { AddcollaboratorService } from './services/addcollaborator.service';
+import { SigninComponent } from './signin/signin.component';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { AddcollaboratorService } from './services/addcollaborator.service';
     SignupComponent,
     TasklistComponent,
     UserComponent,
-    UsersComponent
+    UsersComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
